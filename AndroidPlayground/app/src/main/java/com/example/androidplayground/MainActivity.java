51b7cf62399dc2c8a29f45a06a6664219f46a6e3
jericNuez <jerictolibasnuez@gmail.com>
2023-10-14 08:47:38 +0800

    added android list view

---
+++ AndroidPlayground/app/src/main/java/com/example/androidplayground/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.androidplayground;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String[] heroes = {"Superman", "Iron Man", "Batman", "Spider Man", "Flash"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,heroes);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "You selected: " + heroes[i], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
